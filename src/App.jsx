1ad3cbaa83f14387607fc887246c4114bd47c107
Вероника Мухаметзянова <veronikamuhametzyanova@MacBook-Pro-Veronika.local>
2025-02-21 14:33:43 +0300

    use js

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,70 @@
-import { useState } from 'react';
+import { useState, createElement, Fragment } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-
 export const App = () => {
 	const [count, setCount] = useState(0);
-
-	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-			<div>{new Date().getFullYear()}</div>
-		</>
+	return createElement(
+		Fragment,
+		null,
+		createElement(
+			'div',
+			null,
+			createElement(
+				'a',
+				{
+					href: 'https://vite.dev',
+					target: '_blank',
+				},
+				createElement('img', {
+					src: viteLogo,
+					className: 'logo',
+					alt: 'Vite logo',
+				}),
+			),
+			createElement(
+				'a',
+				{
+					href: 'https://react.dev',
+					target: '_blank',
+				},
+				createElement('img', {
+					src: reactLogo,
+					className: 'logo react',
+					alt: 'React logo',
+				}),
+			),
+		),
+		createElement('h1', null, 'Vite + React'),
+		createElement(
+			'div',
+			{
+				className: 'card',
+			},
+			createElement(
+				'button',
+				{
+					onClick: () => setCount((count) => count + 1),
+				},
+				'count is ',
+				count,
+			),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.jsx'),
+				' and save to test HMR',
+			),
+		),
+		createElement(
+			'p',
+			{
+				className: 'read-the-docs',
+			},
+			'Click on the Vite and React logos to learn more',
+		),
+		createElement('div', null, new Date().getFullYear()),
 	);
-}
-// код написан в декларативном стиле
+};
+// код написан в императивном стиле
